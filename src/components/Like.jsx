095398431d74e753972a1e3eabb14530c322783c
biswajitde9 <biswajitde487@gmail.com>
2023-12-01 17:32:34 +0530

    Toggle Like button for like dislike

--- src/components/Like.jsx
+++ src/components/Like.jsx
@@ -1,18 +1,26 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { FcLike } from "react-icons/fc";
+import { FcLike } from 'react-icons/fc';
 
 const LikeButton = () => {
   const [likes, setLikes] = useState(0);
+  const [isLiked, setIsLiked] = useState(false);
 
   const handleLikeClick = () => {
-    setLikes((prevLikes) => prevLikes + 1);
+    if (isLiked) {
+      setLikes((prevLikes) => prevLikes - 1);
+    } else {
+      setLikes((prevLikes) => prevLikes + 1);
+    }
+
+    // Toggle the isLiked state
+    setIsLiked((prevIsLiked) => !prevIsLiked);
   };
 
   return (
     <div className="mt-3">
-      <button className="btn btn-primary" onClick={handleLikeClick}>
-      <FcLike />{likes > 0 && `(${likes})`}
+      <button className={`btn ${isLiked ? 'btn-danger' : 'btn-primary'}`} onClick={handleLikeClick}>
+        <FcLike /> {likes > 0 && `(${likes})`} {isLiked ? 'Dislike' : 'Like'}
       </button>
     </div>
   );
